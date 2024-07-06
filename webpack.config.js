a0433e7f424beb93fafd97605eaf4a4a7d2e5294
shahirneopraxis <shahir.farhan@neopraxis.in>
2024-07-06 00:04:00 -0700

    webpack updated

--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,33 @@ module.exports = {
     mode: 'production',
     entry: './src/MainPopup.js',
     output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'MainPopup.bundle.[chunkhash].js',
-      library: 'MainPopup',
-      libraryTarget: 'umd',
-      umdNamedDefine: true,
-      globalObject: 'this'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'MainPopup.bundle.[contenthash].js',
+        library: 'MainPopup',
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
+        globalObject: 'this'
     },
     module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: [
-                ['@babel/plugin-transform-react-jsx', { pragma: 'React.createElement' }],
-                '@babel/plugin-transform-react-jsx-self',
-              ]
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: [
+                            ['@babel/plugin-transform-react-jsx', { pragma: 'React.createElement' }],
+                            '@babel/plugin-transform-react-jsx-self'
+                        ]
+                    }
+                }
             },
-          },
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
-        },
-      ],
-    },
-  };
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+};
